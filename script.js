ad2ed6fb7aea79907e6516f909a74d61e1aaa70d
DEMON <Demonfood520@gmail.com>
2024-06-14 11:26:11 -0400

    Add project files.

---
+++ script.js
@@ -0,0 +1,31 @@
+function loadContent(page) {
+    fetch(page)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok ' + response.statusText);
+            }
+            return response.text();
+        })
+        .then(html => {
+            document.getElementById('content').innerHTML = html;
+        })
+        .catch(error => {
+            console.error('There was a problem with the fetch operation:', error);
+            window.location.href = '/Error.html';
+        });
+}
+
+function displayErrorMessage(message) {
+    const errorMessage = document.createElement('div');
+    errorMessage.classList.add('error-message');
+    errorMessage.textContent = `Something went wrong: ${message}`;
+    document.body.appendChild(errorMessage);
+}
+
+
+
+window.onload = function() {
+    if (window.location.pathname === '/' || window.location.pathname === '') {
+        loadContent('Home.html');
+    }
+};
