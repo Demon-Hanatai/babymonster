85dcbaa8ecdcf322c26a9f3db1188475b42e2318
Demon-Hanatai <109008640+Demon-Hanatai@users.noreply.github.com>
2024-06-14 11:43:04 -0400

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function loadContent(page) {
         })
         .catch(error => {
             console.error('There was a problem with the fetch operation:', error);
-            window.location.href = '/Error.html';
+            window.location.href = 'Error.html';
         });
 }
 
